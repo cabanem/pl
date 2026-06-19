9d9ae4801e8bda2e0910a8797cca3c9774dc0aab
EC <cabanem@pm.me>
2026-06-19 14:57:12 -0400

    Refactor REPL script to use new constants and logic

--- workato_cli/1.0.0/00_repl.py
+++ workato_cli/1.0.0/00_repl.py
@@ -1,4 +1,4 @@
-PYTHONPATH=sdc_recipe_model.py python - <<'PY' >> output.txt
+PYTHONPATH=/path/to/sdc-recipe-model python3 - <<'PY'
 import os, json
 from collections import defaultdict
 from workato_client import WorkatoClient, WorkatoConfig, safe_parse_json, load_dotenv
@@ -9,18 +9,20 @@ client = WorkatoClient(config=WorkatoConfig.from_env())
 folder = os.environ["SDC_FOLDER_ID"]
 rf = os.environ.get("SDC_RECIPES_FOLDER_ID") or _resolve_scope_folder(client, folder, "Recipes")
 
-WANT = {("workato_api_platform","return_response"), ("workato_workflow_task","app_function_return")}
+CAP = {"store_file":2, "get_file_contents":2, "ensure_dir_exists":2, "create_shareable_link":2,
+       "delete_file":1, "delete_directory":1, "append_to_file":1, "search_files":1}
 samples = defaultdict(list)
 for r in client.list_recipes(folder_id=rf):
     if str(r.get("folder_id")) != str(rf): continue
-    if all(len(samples[k]) >= 2 for k in WANT): break
+    if all(len(samples[k]) >= v for k, v in CAP.items()): break
     code = safe_parse_json(client.get_recipe(r["id"])["code"])
     for n in walk_steps(code):
-        key = (n.get("provider"), n.get("name"))
-        if key in WANT and len(samples[key]) < 2:
-            samples[key].append({k: n.get(k) for k in
-                ("provider","name","input","extended_output_schema","extended_input_schema")})
-for key, exs in samples.items():
-    print("="*60, key)
-    for ex in exs: print(json.dumps(ex, indent=2)[:2000])
+        if n.get("provider") == "workato_files":
+            nm = n.get("name")
+            if len(samples[nm]) < CAP.get(nm, 1):
+                samples[nm].append({"name": nm, "input": n.get("input")})
+for nm, exs in samples.items():
+    print("="*60, nm)
+    for ex in exs:
+        print(json.dumps(ex, indent=2)[:1500])
 PY
